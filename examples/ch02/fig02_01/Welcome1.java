2e2216a2415256c4a23a8d03b50002c51bb338d6
Zhou Jing <transbot@gmail.com>
2025-06-02 07:21:07 +0800

    Chapter 2 finished

--- examples/ch02/fig02_01/Welcome1.java
+++ examples/ch02/fig02_01/Welcome1.java
@@ -1,11 +1,10 @@
-// Welcome1.java
 // 图2.1: Welcome1.java
 // 文本输出程序
 
 public class Welcome1 { 
    // main方法开始程序执行
    public static void main(String[] args) {
-      System.out.println("Welcome to Java Programming!");
+      System.out.println("欢迎学习Java编程!");
    } // 结束main方法
 } // 结束Welcome1类
 
