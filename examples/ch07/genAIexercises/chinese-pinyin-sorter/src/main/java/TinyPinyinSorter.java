66e1f2f386d43d0da554f9f06f93b7bd27ad613b
Zhou Jing <transbot@gmail.com>
2025-06-28 01:58:28 +0800

    finished Chapter 7, add genAI exercises for Chinese Edition

--- examples/ch07/genAIexercises/chinese-pinyin-sorter/src/main/java/TinyPinyinSorter.java
+++
@@ -1,6 +0,0 @@
-public class TinyPinyinSorter {
-    public static void main(String[] args) {
-        System.out.println("TinyPinyin 初始化状态: " + com.github.promeg.pinyinhelper.Pinyin.isInitialized());
-        // ... 其余代码 ...
-    }
-}
